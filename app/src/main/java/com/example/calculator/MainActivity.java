acde6a089e7df64a9855521127f27706fd2db84c
cmy <1139401422@qq.com>
2024-06-04 11:42:20 +0800

    成功在计算器上方添加OptionMenu

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.calculator;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -17,4 +21,31 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    /**
+     * 添加OptionMenu
+     * @param menu The options menu in which you place your items.
+     *
+     * @return
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.my_menu, menu);
+        return true;
+    }
+
+    /**
+     * 处理选项菜单Optionmenu点击事件
+     * @param item The menu item that was selected.
+     *
+     * @return
+     */
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int itemId = item.getItemId();
+        if (itemId == R.id.item_history) {
+            Toast.makeText(this, "你点击了历史记录", Toast.LENGTH_SHORT).show();
+        }
+
+        return true;
+    }
 }
