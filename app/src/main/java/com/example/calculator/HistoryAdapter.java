c004f1b166a99c12ea22ebabdc6a0933dcf9186b
cmy <1139401422@qq.com>
2024-06-06 11:19:30 +0800

    定义了一个广播接收器，用于接收点击历史记录item时发出的广播

--- app/src/main/java/com/example/calculator/HistoryAdapter.java
+++ app/src/main/java/com/example/calculator/HistoryAdapter.java
@@ -1,10 +1,12 @@
 package com.example.calculator;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -13,6 +15,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class HistoryAdapter extends RecyclerView.Adapter<HistoryAdapter.ViewHolder> {
+    private static final String TAG = "HistoryAdapter";
     private ArrayList<CalculationHistory> historyList;
 
     public HistoryAdapter(ArrayList<CalculationHistory> calculationHistoryList) {
@@ -34,8 +37,12 @@ public class HistoryAdapter extends RecyclerView.Adapter<HistoryAdapter.ViewHold
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.history_item, parent, false);
-        final ViewHolder viewHolder = new ViewHolder(view);
-        return viewHolder;
+        final ViewHolder holder = new ViewHolder(view);
+        holder.itemView.setOnClickListener(v -> {
+            Log.d(TAG, "你点击了！！！");
+
+        });
+        return holder;
     }
 
     @Override
